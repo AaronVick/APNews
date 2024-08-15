9ab49a0270e264f3019dfcf85ba9168fdf2c201c
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-15 07:55:50 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,15 @@ const Home = ({ baseUrl, imageUrl }) => {
         <title>AP Top News Farcaster Frame</title>
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={imageUrl} />
+        
         <meta property="fc:frame:button:1" content="Show Top Headlines" />
         <meta property="fc:frame:post_url" content={`${baseUrl}/api/handleAction`} />
+
+        <meta property="fc:frame:button:2" content="Share" />
+        <meta property="fc:frame:button:2:action" content="link" />
+        <meta property="fc:frame:button:2:target" content="https://warpcast.com/~/compose?text=Trending+News+from+the+Associated+Press%0A%0Aframe+by+%40aaronv&embeds[]=https%3A%2F%2Fap-news.vercel.app%2F" />
+
+
       </Head>
       <div>
         <h1>AP Top News</h1>
